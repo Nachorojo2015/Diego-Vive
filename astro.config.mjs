45a4dc0f26469897c5877307c2ada811ac516aad
Ignacio Gomez <Nachorojo2015@gmail.com>
2024-07-08 16:14:10 -0300

    delete-changes-vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@vercel/analytics";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind()],
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 });
